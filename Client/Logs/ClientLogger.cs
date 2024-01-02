b0b375a6a71c44aa6039ef94957aff9e7ff3e702
Data Bank <drdatabankwork@gmail.com>
2024-01-02 17:30:35 +0100

    Add Logs

---
+++ Client/Logs/ClientLogger.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+
+namespace Client.Logs
+{
+    internal class ClientLogger
+    {
+        public Logger Logger { get; set; }
+        public Logger ReportLoger { get; set; }
+
+        public ClientLogger()
+        {
+            LogManager.LoadConfiguration("../../../Logs/nlog.config");
+            Logger = LogManager.GetCurrentClassLogger();
+            ReportLoger = LogManager.GetLogger("Reports");
+
+        }
+    }
+}
---
+++ Server/Logs/ServerLogger.cs
@@ -0,0 +1,16 @@
+﻿using NLog;
+
+namespace Server.Logs
+{
+    internal class ServerLogger
+    {
+        public Logger Logger { get; set; }
+        public Logger ReportLoger { get; set; }
+
+        public ServerLogger() {
+            LogManager.LoadConfiguration("../../../Logs/nlog.config");
+            Logger = LogManager.GetCurrentClassLogger();
+            ReportLoger = LogManager.GetLogger("Reports");  
+        }   
+    }
+}
